cff64c292e7eba48a311ec5d16111e023caaa4bf
dplong <longdo805@gmail.com>
2025-08-13 08:19:19 +0700

    Revert "feat: add personal info"
    This reverts commit abdcb97de9c2bbccb799bde140f2642b1fc664db.

--- lib/constants.dart
+++
@@ -1,6 +0,0 @@
-const developerName = 'Hirokito';
-const myName = 'Đỗ Phi Long';
-final dateOfBirth = DateTime(1997, 10, 10);
-const emailContact = 'longdo805@gmaill.com';
-const phoneNumber = '(+84) 388 123 095';
-const githubUrl = 'https://github.com/PhiLongDo';
